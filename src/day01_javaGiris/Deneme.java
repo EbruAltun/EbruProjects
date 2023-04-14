16a835112f81a3edb2629c50600d1fc4075b40bb
Ebru2535 <ebrusavas.2015@gmail.com>
2023-04-15 00:41:12 +0300

    deneme 3

--- src/day01_javaGiris/Deneme.java
+++ src/day01_javaGiris/Deneme.java
@@ -3,6 +3,7 @@ package day01_javaGiris;
 public class Deneme {
     public static void main(String[] args) {
         System.out.println("Hello Githup");
+        System.out.println("ebru");
 
     }
 }
